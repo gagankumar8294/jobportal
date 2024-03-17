35fb78bb2714918c128df3d60627293ecedfdd05
Gagan-Sky <YOUR_EMAIL>
2024-03-17 20:20:12 +0530

    added all the code to controller

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import express from 'express';
-import HomepageController from './src/controllers/home.controller.js'
+import HomepageController from "./src/controllers/home.controller.js";
 
 const server = express();
 
 const homeController = new HomepageController()
 
-server.get('/', (req, res) => {
-    return res.send("welcome to Job portal")
-});
-
+server.get('/', homeController.getHomepage);
+server.use(express.static('src/views'))
 server.listen(3000);
-
-console.log("server listening on port 3000 https://localhost:3000");
+console.log("server listening on port 3000 http://localhost:3000");
--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,6 +1,7 @@
 import path from 'path';
 
 export default class HomepageController {
+    
     getHomepage(req, res) {
         return res.sendFile(path.join(path.resolve(), 'src', 'views','index.html'));
     }
