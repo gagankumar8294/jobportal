1ecea52661a18cb3d95e27d28363ee2160dc3a3a
Gagan-Sky <YOUR_EMAIL>
2024-03-23 09:04:03 +0530

    added validation

--- src/controllers/applicants.controller.js
+++ src/controllers/applicants.controller.js
@@ -21,10 +21,36 @@ export default class ApplicantsController {
     }
 
     renderPostJob(req, res){
-        return res.render('postjob');
+        return res.render('postjob', {errorMessage: null,});
     }
 
     createPostJob(req, res){
+
+        const {location, company, salary, positions, category, designation } = req.body;
+        let errors=[];
+        if(!location || location.trim() ==''){
+            errors.push("location is required");
+        }
+        if(!company || company.trim() ==''){
+            errors.push("company Name is required");
+        }
+        if(!salary || parseFloat(salary)<1){
+            errors.push("Salary must be a positive vale");
+        }
+        if(!positions || parseFloat(positions)<1){
+            errors.push("Positions must be a positive vale");
+        }
+        if (!category || category.trim() == '') {
+            errors.push("Job category is required");
+        }
+        if (!designation || designation.trim() == '') {
+            errors.push("Job designation is required");
+        }
+
+        if(errors.length>0) {
+            return res.render('postjob', {errorMessage:errors[0]})
+        }
+
         console.log(req.body);
         JobsModel.add(req.body);
         let jobs = JobsModel.get();
